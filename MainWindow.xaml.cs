8ba086524c6b236046343454ec5282e5fc6beb5c
Yacine BENANNOUNE <yacine.benannoune@gmail.com>
2016-09-15 20:02:52 +0200

    Fixed ssdp discovery issue by using another method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
 using MahApps.Metro.Controls;
-using Xceed.Wpf.Toolkit;
+using System.Diagnostics.Contracts;
+
+// SSDP reference : https://searchcode.com/codesearch/view/8152089/ /DeviceFinder.SSDP.cs 
+// https://github.com/arktronic/ssdp-discover/blob/master/Program.cs
 
 namespace YeelightController
 {
@@ -31,10 +28,25 @@ namespace YeelightController
         private Bulb m_ConnectedBulb;
         //TcpClient used to communicate with the Bulb
         private TcpClient m_TcpClient;
-        //MultiCastAddress
-        private IPAddress m_MultiCastAddress = IPAddress.Parse("239.255.255.250");    
+
+        //Socket for SSDP 
+        private Socket _ssdpSocket;
+        private byte[] _ssdpReceiveBuffer;
+
         //Udp port used during the ssdp discovers
-        private int m_Port = 1982;    
+        private static readonly int m_Port = 1982;
+
+        //MultiCastEndPoint 
+        private static readonly IPEndPoint MulticastEndPoint = new IPEndPoint(IPAddress.Parse("239.255.255.250"), m_Port);
+        private static readonly IPEndPoint AnyEndPoint = new IPEndPoint(IPAddress.Any, 0);        
+
+        //SSDP Diagram Message
+        private static readonly string ssdpMessage = "M-SEARCH * HTTP/1.1\r\n" +
+                                                     "HOST: 239.255.255.250:1982\r\n" +
+                                                     "MAN: \"ssdp:discover\"\r\n" +
+                                                     "ST: wifi_bulb";
+
+        private static readonly byte[] dgram = Encoding.ASCII.GetBytes(ssdpMessage);
 
         public MainWindow()
         {
@@ -44,7 +56,7 @@ namespace YeelightController
             lstBulbs.ItemsSource = m_Bulbs;
 
             //Search bulbs once at running time
-            DiscoverBulbs();                 
+            StartListening();                 
         }
 
         /// <summary>
@@ -71,79 +83,112 @@ namespace YeelightController
             }
             return false;
         }
+          
 
+        #region HandleResponse
+
+        private void HandleResponse(EndPoint sender, string response)
+        {
+            Contract.Requires(sender != null);
+            Contract.Requires(response != null);
+
+            string ip = "";
+            GetSubString(response, "Location: yeelight://", ":", ref ip);
+
+            //if list already contains this bulb skip
+            bool alreadyExisting = m_Bulbs.Any(item => item.Ip == ip);
+            if (alreadyExisting == false)
+            {
+                string id = "";
+                GetSubString(response, "id: ", "\r\n", ref id);
+
+                string bright = "";
+                GetSubString(response, "bright: ", "\r\n", ref bright);
+
+                string power = "";
+                GetSubString(response, "power: ", "\r\n", ref power);
+                bool isOn = power.Contains("on");
+
+                m_Bulbs.Add(new Bulb(ip, id, isOn, Convert.ToInt32(bright)));
+            }
+        }
+             
+        private void ReceiveResponseRecursive(IAsyncResult r = null)
+        {
+            Contract.Requires(r == null || _ssdpReceiveBuffer != null);
+            Contract.Ensures(_ssdpReceiveBuffer != null);
+
+            // check if there is an response
+            // or this is the first call
+            if (r != null)
+            {
+                // complete read
+                EndPoint senderEndPoint = AnyEndPoint;
+
+                var read = _ssdpSocket.EndReceiveFrom(
+                    r,
+                    ref senderEndPoint
+                    );
+
+                // parse result
+                var resBuf = _ssdpReceiveBuffer;    // make sure we don't reuse the reference
+
+                new Task(
+                    () => HandleResponse(senderEndPoint, Encoding.ASCII.GetString(resBuf, 0, read))
+                    ).Start();
+            }
+
+            // trigger the next cycle
+            // tail recursion
+            EndPoint recvEndPoint = AnyEndPoint;
+            _ssdpReceiveBuffer = new byte[4096];
+
+            _ssdpSocket.BeginReceiveFrom(
+                _ssdpReceiveBuffer,
+                0,
+                _ssdpReceiveBuffer.Length,
+                SocketFlags.None,
+                ref recvEndPoint,
+                ReceiveResponseRecursive,
+                null
+            );
+        }
+
+        #endregion
 
         /// <summary>
         /// Function asynchrone to discovers all buls in the network
         /// Discovered bulbs are added to the list on the MainWindow
         /// </summary>
-        private async void DiscoverBulbs()
-        {     
-            UdpClient client = new UdpClient();           
-
-            //convert to byte array
-            byte[] dgram = Encoding.ASCII.GetBytes("M-SEARCH * HTTP/1.1\r\n" +
-                      "HOST: 239.255.255.250:1982\r\n" +
-                      "MAN: \"ssdp:discover\"\r\n" +
-                      "ST: wifi_bulb");
-
-            //Remote endpoint "239.255.255.250:1982"
-            IPEndPoint remoteEndPoint = new IPEndPoint(m_MultiCastAddress, m_Port);
-
-            //Join multicastgroup
-            client.JoinMulticastGroup(m_MultiCastAddress);
-
-            //Send the message
-            client.Send(dgram, dgram.Length, remoteEndPoint);
-
-            //After 1000 ms timeout we stop research
-            client.Client.ReceiveTimeout = 1000;
-
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);   
-            
-                     
-            await Task.Run(() =>
+        private void StartListening()
+        {         
+            // create socket
+            _ssdpSocket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Dgram,
+                    ProtocolType.Udp
+                    )            
             {
-                try
-                {
-                    while (true)
-                    {             
-                        byte[] receiveBytes = client.Receive(ref RemoteIpEndPoint);
-                        string message = Encoding.ASCII.GetString(receiveBytes);
-                                     
-                        string ip = "";
-                        GetSubString(message, "Location: yeelight://", ":", ref ip);
+                Blocking = false,
+                Ttl = 1,
+                UseOnlyOverlappedIO = true,
+                MulticastLoopback = false,
+            };
 
-                        //if list already contains this bulb skip
-                        bool alreadyExisting = m_Bulbs.Any(item => item.Ip == ip);
-                        if (alreadyExisting == false)
-                        {
-                            string id = "";
-                            GetSubString(message, "id: ", "\r\n", ref id);
+            _ssdpSocket.Bind(AnyEndPoint);
 
-                            string bright = "";
-                            GetSubString(message, "bright: ", "\r\n", ref bright);
+            _ssdpSocket.SetSocketOption(
+              SocketOptionLevel.IP,
+              SocketOptionName.AddMembership,
+              new MulticastOption(MulticastEndPoint.Address)
+            );      
 
-                            string power = "";
-                            GetSubString(message, "power: ", "\r\n", ref power);
-                            bool isOn = power.Contains("on");
+            // start listening
+            ReceiveResponseRecursive();
 
-                            m_Bulbs.Add(new Bulb(ip, id, isOn, Convert.ToInt32(bright)));
-                        }              
-                    }
-                }
-                catch
-                {                 
-                    //timeout
-                }
-                finally
-                {
-                    client.Close();
-                }          
-            });          
+            _ssdpSocket.SendTo(dgram, MulticastEndPoint);   
         }
-             
-
+     
         private void lstBulbs_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (m_TcpClient != null)
@@ -171,7 +216,8 @@ namespace YeelightController
                 //Change panel state -> allow modification
                 panelBulbControl.IsEnabled = true;
             }
-        }              
+        }       
+
         
         private void btnToggle_IsCheckedChanged(object sender, EventArgs e)
         {
